1b7bade7f5b1e96027f8bf5b7c082f97e3729a9f
shaitao <sht892197>
2022-02-28 16:49:52 +0800

    Fast sync before subscribe.

--- scanner/src/main.rs
+++ scanner/src/main.rs
@@ -25,6 +25,17 @@ async fn main() -> Result<()> {
     match commands::Scanner::parse() {
         Scanner::Load(load) => load.execute().await,
         Scanner::Scan(batch_scan) => batch_scan.execute().await,
-        Scanner::Subscribe(subscribe) => subscribe.run().await,
+        Scanner::Subscribe(subscribe) => {
+            if let Err(e) = subscribe.run().await {
+                error!("{:?}", e);
+                println!("Existing...");
+                // EX_SOFTWARE (70) : An internal software error has been detected. This
+                // should be limited to	non-operating system related
+                // errors as possible.
+                std::process::exit(70);
+            }
+            //for type check, meaningless.
+            Ok(())
+        }
     }
 }
