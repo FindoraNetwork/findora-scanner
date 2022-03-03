f58d7d75c69a4eecea7fd1e1ee824d35d885b1db
shaitao <sht892197>
2022-03-03 15:02:39 +0800

    fix logger target.

--- scanner/src/main.rs
+++ scanner/src/main.rs
@@ -7,8 +7,8 @@ pub use error::*;
 pub mod commands;
 pub mod db;
 pub mod rpc;
-pub mod tx;
 pub mod scanner;
+pub mod tx;
 
 use clap::Parser;
 
@@ -17,7 +17,9 @@ use commands::Scanner;
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv::dotenv().ok();
-    env_logger::init();
+    env_logger::builder()
+        .target(env_logger::Target::Stdout)
+        .init();
     match commands::Scanner::parse() {
         Scanner::Load(load) => load.execute().await,
         Scanner::Scan(batch_scan) => batch_scan.execute().await,
