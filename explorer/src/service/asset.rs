4cbe3e42f75ead7ac90538d71222f0a00fca6666
chensiqi_scdy@163.com <chensiqi_scdy@163.com>
2022-07-14 01:46:07 -0700

    parse issuer address.

--- explorer/src/service/asset.rs
+++ explorer/src/service/asset.rs
@@ -1,9 +1,14 @@
 use crate::Api;
 use anyhow::Result;
+use module::utils::crypto::bech32enc;
 use poem_openapi::{param::Path, payload::Json, ApiResponse, Object};
+use ruc::{d, RucResult};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use sqlx::Row;
+use zei::serialization::ZeiFromToBytes;
+use zei::xfr::sig::XfrPublicKey;
+
 #[derive(ApiResponse)]
 pub enum AssetResponse {
     #[oai(status = 200)]
@@ -14,7 +19,7 @@ pub enum AssetResponse {
 pub struct AssetRes {
     pub code: i32,
     pub message: String,
-    pub data: Option<Asset>,
+    pub data: Option<AssetDisplay>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Default, Object)]
@@ -41,6 +46,14 @@ pub struct Asset {
     pub asset_rules: AssetRules,
     pub code: Code,
     pub issuer: PubKey,
+    pub memo: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default, Object)]
+pub struct AssetDisplay {
+    pub asset_rules: AssetRules,
+    pub code: Code,
+    pub issuer: String,
     pub memo: String,
 }
 
@@ -83,13 +96,20 @@ pub async fn get_asset(api: &Api, address: Path<String>) -> Result<AssetResponse
         }
     };
 
-    let mut asset = Asset::default();
+    let mut asset = AssetDisplay::default();
     for row in rows {
         let v: Value = row.try_get("asset").unwrap();
         let a: Asset = serde_json::from_value(v).unwrap();
+
         if a.code.val.eq(&code) {
+            let pk = base64::decode_config(&a.issuer.key, base64::URL_SAFE)
+                .c(d!())
+                .and_then(|bytes| XfrPublicKey::zei_from_bytes(&bytes).c(d!()))
+                .unwrap();
+            let issuer_addr = bech32enc(&XfrPublicKey::zei_to_bytes(&pk));
+
             asset.memo = a.memo;
-            asset.issuer = a.issuer;
+            asset.issuer = issuer_addr;
             asset.code = a.code;
             asset.asset_rules = a.asset_rules;
         }
--- module/src/utils/crypto.rs
+++ module/src/utils/crypto.rs
@@ -1,3 +1,4 @@
+use bech32::ToBase32;
 use ethereum::{LegacyTransaction, LegacyTransactionMessage};
 use ethereum_types::{H160, H256};
 use ruc::eg;
@@ -37,11 +38,19 @@ pub fn secp256k1_ecdsa_recover(sig: &[u8; 65], msg: &[u8; 32]) -> ruc::Result<[u
     Ok(res)
 }
 
+#[inline(always)]
+pub fn bech32enc<T: AsRef<[u8]> + ToBase32>(input: &T) -> String {
+    bech32::encode("fra", input.to_base32()).unwrap()
+}
+
 #[cfg(test)]
 #[allow(missing_docs)]
 mod test {
     use super::*;
     use crate::schema::EvmTx;
+    use ruc::{d, RucResult};
+    use zei::serialization::ZeiFromToBytes;
+    use zei::xfr::sig::XfrPublicKey;
 
     #[test]
     fn test_recover_signer() {
@@ -51,5 +60,21 @@ mod test {
         let signer = recover_signer(&evm_tx.function.ethereum.transact).unwrap();
         let addr = format!("{:?}", signer);
         assert_eq!(addr, "0xa5225cbee5052100ec2d2d94aa6d258558073757");
+    }
+
+    #[test]
+    fn test_convert_base64_to_bech32() {
+        let pk = base64::decode_config(
+            "HZnxwPI5PD_xpQX1NqKTHXqPdHXVXtGe7yQ0JI3MVTs=",
+            base64::URL_SAFE,
+        )
+        .c(d!())
+        .and_then(|bytes| XfrPublicKey::zei_from_bytes(&bytes).c(d!()))
+        .unwrap();
+        let addr = bech32enc(&XfrPublicKey::zei_to_bytes(&pk));
+        assert_eq!(
+            addr,
+            "fra1rkvlrs8j8y7rlud9qh6ndg5nr4ag7ar4640dr8h0ys6zfrwv25as42zptu"
+        );
     }
 }
