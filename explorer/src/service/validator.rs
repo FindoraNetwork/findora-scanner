59ca8abe0b17ed152338223cb95533609daee755
chensiqi_scdy@163.com <chensiqi_scdy@163.com>
2022-08-12 02:08:42 -0700

    implement validator_delegation.

--- explorer/src/service/validator.rs
+++ explorer/src/service/validator.rs
@@ -2,7 +2,7 @@ use crate::Api;
 use anyhow::Result;
 use log::debug;
 use module::schema::Memo;
-use poem_openapi::param::Path;
+use poem_openapi::param::{Path, Query};
 use poem_openapi::{payload::Json, ApiResponse, Object};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
@@ -31,16 +31,24 @@ pub enum DelegatorListResponse {
     Ok(Json<DelegatorListResult>),
 }
 
+#[derive(ApiResponse)]
+pub enum ValidatorDelegationResponse {
+    #[oai(status = 200)]
+    Ok(Json<ValidatorDelegationResult>),
+}
+
+#[derive(Serialize, Deserialize, Debug, Default, Object)]
+pub struct ValidatorDelegationResult {
+    pub code: i32,
+    pub message: String,
+    pub data: Option<Value>,
+}
+
 #[derive(Serialize, Deserialize, Debug, Default, Object)]
 pub struct DelegatorListResult {
     pub code: i32,
     pub message: String,
-    pub data: Option<DelegatorList>,
-}
-
-#[derive(Serialize, Deserialize, Debug, Default, Object)]
-pub struct DelegatorList {
-    pub delegators: Vec<DelegatorItem>,
+    pub data: Option<Value>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Default, Object)]
@@ -53,55 +61,21 @@ pub struct DelegatorItem {
 pub struct CirculatingSupplyResult {
     pub code: i32,
     pub message: String,
-    pub data: Option<CirculatingSupply>,
-}
-
-#[derive(Serialize, Deserialize, Debug, Default, Object)]
-pub struct CirculatingSupply {
-    pub global_circulating_supply: f64,
-    pub global_delegation_amount: f64,
-    pub global_return_rate: f64,
+    pub data: Option<Value>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Default, Object)]
 pub struct ValidatorDetailResult {
     pub code: i32,
     pub message: String,
-    pub data: Option<ValidatorDetail>,
-}
-
-#[derive(Serialize, Deserialize, Debug, Default, Object)]
-pub struct ValidatorDetail {
-    pub addr: String,
-    pub kind: String,
-    pub is_online: bool,
-    pub voting_power: i64,
-    pub voting_power_rank: i64,
-    pub commission_rate: Vec<i64>,
-    pub self_staking: i64,
-    pub fra_rewards: i64,
-    pub memo: Memo,
-    pub start_height: i64,
-    pub cur_height: i64,
-    pub block_signed_cnt: i64,
-    pub block_proposed_cnt: i64,
-    pub validator_realtime_apy: Value,
-    pub delegator_cnt: i64,
+    pub data: Option<Value>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Default, Object)]
 pub struct ValidatorListResult {
     pub code: i32,
     pub message: String,
-    pub data: Option<ValidatorList>,
-}
-
-#[derive(Serialize, Deserialize, Debug, Default, Object)]
-pub struct ValidatorList {
-    pub threshold: Vec<i64>,
-    pub validator_cnt: i64,
-    pub cur_height: i64,
-    pub validators: Vec<Validator>,
+    pub data: Option<Value>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Default, Object)]
@@ -114,16 +88,45 @@ pub struct Validator {
     pub extra: Memo,
 }
 
+pub async fn validator_delegation(
+    api: &Api,
+    address: Query<String>,
+) -> Result<ValidatorDelegationResponse> {
+    let mut validator_delegation_url = api.platform.rpc.join("validator_delegation").unwrap();
+    validator_delegation_url.set_query(Some(&format!("address={}", address.0)));
+
+    debug!("validator_delegation_url: {}", validator_delegation_url);
+
+    let res = api
+        .platform
+        .client
+        .get(validator_delegation_url)
+        .send()
+        .await?
+        .json()
+        .await?;
+
+    Ok(ValidatorDelegationResponse::Ok(Json(
+        ValidatorDelegationResult {
+            code: 200,
+            message: "".to_string(),
+            data: Some(res),
+        },
+    )))
+}
+
 pub async fn validator_list(api: &Api) -> Result<ValidatorListResponse> {
     let validator_list_url = api.platform.rpc.join("validator_list").unwrap();
+
     debug!("validator_list_url: {}", validator_list_url);
+
     let res = api
         .platform
         .client
         .get(validator_list_url)
         .send()
         .await?
-        .json::<ValidatorList>()
+        .json()
         .await?;
 
     Ok(ValidatorListResponse::Ok(Json(ValidatorListResult {
@@ -139,6 +142,7 @@ pub async fn validator_detail(api: &Api, address: Path<String>) -> Result<Valida
         .rpc
         .join(format!("validator_detail/{}", address.0).as_str())
         .unwrap();
+
     debug!("validator_detail_url: {}", validator_detail_url);
 
     let res = api
@@ -147,7 +151,7 @@ pub async fn validator_detail(api: &Api, address: Path<String>) -> Result<Valida
         .get(validator_detail_url)
         .send()
         .await?
-        .json::<ValidatorDetail>()
+        .json()
         .await?;
 
     Ok(ValidatorDetailResponse::Ok(Json(ValidatorDetailResult {
@@ -163,6 +167,7 @@ pub async fn delegator_list(api: &Api, address: Path<String>) -> Result<Delegato
         .rpc
         .join(format!("delegator_list/{}", address.0).as_str())
         .unwrap();
+
     debug!("delegator_list_url: {}", delegator_list_url);
 
     let res = api
@@ -171,7 +176,7 @@ pub async fn delegator_list(api: &Api, address: Path<String>) -> Result<Delegato
         .get(delegator_list_url)
         .send()
         .await?
-        .json::<DelegatorList>()
+        .json()
         .await?;
 
     Ok(DelegatorListResponse::Ok(Json(DelegatorListResult {
@@ -183,6 +188,7 @@ pub async fn delegator_list(api: &Api, address: Path<String>) -> Result<Delegato
 
 pub async fn circulating_supply(api: &Api) -> Result<CirculatingSupplyResponse> {
     let circulating_supply_url = api.platform.rpc.join("circulating_supply").unwrap();
+
     debug!("circulating_supply_url: {}", circulating_supply_url);
 
     let res = api
@@ -191,7 +197,7 @@ pub async fn circulating_supply(api: &Api) -> Result<CirculatingSupplyResponse> 
         .get(circulating_supply_url)
         .send()
         .await?
-        .json::<CirculatingSupply>()
+        .json()
         .await?;
 
     Ok(CirculatingSupplyResponse::Ok(Json(
