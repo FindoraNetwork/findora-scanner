e090399ddf39ae5b7c6503b38f65755ce73c3cdd
ssuch1ch4n@gmail.com <ssuch1ch4n@gmail.com>
2022-09-18 16:12:19 +0800

    reduce scanning interval.

--- scanner/src/commands.rs
+++ scanner/src/commands.rs
@@ -15,7 +15,7 @@ use crate::{Error, Result};
 const DEFAULT_TIMEOUT_SECS: u64 = 32;
 const DEFAULT_RETIES: usize = 3;
 const DEFAULT_CONCURRENCY: usize = 8;
-const DEFAULT_INTERVER: Duration = Duration::from_secs(15);
+const DEFAULT_INTERVAL: Duration = Duration::from_secs(10);
 
 /// load block at specific height.
 #[derive(Parser, Debug)]
@@ -56,17 +56,8 @@ impl Load {
         };
 
         info!("Got header {}", target);
-
         let caller = RPCCaller::new(retries, 1, timeout, rpc, pool);
-
         caller.load_and_save_block(target).await?;
-
-        // if self.staking {
-        //     match caller.load_and_save_staking().await {
-        //         Ok(h) => info!("Staking at {} loaded.", h),
-        //         Err(e) => warn!("Failed to load staking: {:?}", e),
-        //     }
-        // }
 
         info!("Load block at height {} succeed.", target);
         Ok(())
@@ -155,7 +146,7 @@ impl Subscribe {
         let interval = self
             .interval
             .map(Duration::from_secs)
-            .unwrap_or(DEFAULT_INTERVER);
+            .unwrap_or(DEFAULT_INTERVAL);
         let retries = self.retries.unwrap_or(DEFAULT_RETIES);
 
         let mut cursor = if let Some(h) = self.start {
@@ -170,20 +161,17 @@ impl Subscribe {
         };
 
         let concurrency = self.concurrency.unwrap_or(DEFAULT_CONCURRENCY);
-
         assert!(concurrency >= 1);
-
         let range_scanner = RangeScanner::new(timeout, rpc, retries, concurrency, pool.clone());
-
-        let bacth_size = 4 * concurrency as i64;
+        let batch_size = 4 * concurrency as i64;
 
         info!("Subscribing start from {}, try fast sync ...", cursor);
         loop {
             let succeed_cnt = range_scanner
-                .range_scan(cursor, cursor + bacth_size)
+                .range_scan(cursor, cursor + batch_size)
                 .await?;
-            if succeed_cnt == bacth_size {
-                cursor += bacth_size;
+            if succeed_cnt == batch_size {
+                cursor += batch_size;
             } else {
                 break;
             }
@@ -197,12 +185,6 @@ impl Subscribe {
             match caller.load_and_save_block(cursor).await {
                 Ok(_) => {
                     info!("Block at {} loaded.", cursor);
-                    // if self.staking {
-                    //     match caller.load_and_save_staking().await {
-                    //         Ok(h) => info!("Staking at {} loaded.", h),
-                    //         Err(e) => warn!("Failed to load staking: {:?}", e),
-                    //     }
-                    // }
                 }
                 Err(Error::NotFound) => (),
                 Err(e) => return Err(e),
