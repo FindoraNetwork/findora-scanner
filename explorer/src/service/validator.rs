e74520477121c04d77ea0da01c371cb2648f23bf
陳振華 <ssuchichan@gmail.com>
2022-10-09 09:36:45 +0800

    add online in validator response. (#99)

--- explorer/src/service/validator.rs
+++ explorer/src/service/validator.rs
@@ -6,6 +6,7 @@ use poem_openapi::param::{Path, Query};
 use poem_openapi::{payload::Json, ApiResponse, Object};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
+use sqlx::Row;
 
 #[derive(ApiResponse)]
 pub enum ValidatorListResponse {
@@ -75,17 +76,74 @@ pub struct ValidatorDetailResult {
 pub struct ValidatorListResult {
     pub code: i32,
     pub message: String,
-    pub data: Option<Value>,
+    pub data: Option<WrapValidatorListData>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default, Object)]
+pub struct WrapValidatorListData {
+    pub threshold: [i64; 2],
+    pub validator_cnt: i64,
+    pub cur_height: i64,
+    pub validators: Vec<WrapValidator>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default, Object)]
+pub struct WrapValidator {
+    pub addr: String,
+    pub power: i64,
+    pub commission_rate: [i64; 2],
+    pub accept_delegation: bool,
+    pub online: bool,
+    pub rank: i64,
+    pub extra: Memo,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default, Object)]
+pub struct ValidatorListData {
+    pub threshold: [i64; 2],
+    pub validator_cnt: i64,
+    pub cur_height: i64,
+    pub validators: Vec<Validator>,
+}
+
+impl ValidatorListData {
+    fn wrap(self) -> WrapValidatorListData {
+        let mut wrap_validators: Vec<WrapValidator> = vec![];
+        for v in self.validators {
+            wrap_validators.push(v.wrap());
+        }
+
+        WrapValidatorListData {
+            threshold: self.threshold,
+            validator_cnt: self.validator_cnt,
+            cur_height: self.cur_height,
+            validators: wrap_validators,
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, Default, Object)]
 pub struct Validator {
     pub addr: String,
     pub power: i64,
-    pub commission_rate: Vec<i64>,
+    pub commission_rate: [i64; 2],
     pub accept_delegation: bool,
     pub rank: i64,
     pub extra: Memo,
+}
+
+impl Validator {
+    fn wrap(self) -> WrapValidator {
+        WrapValidator {
+            addr: self.addr,
+            power: self.power,
+            commission_rate: self.commission_rate,
+            accept_delegation: self.accept_delegation,
+            online: false,
+            rank: self.rank,
+            extra: self.extra,
+        }
+    }
 }
 
 pub async fn validator_delegation(
@@ -116,8 +174,9 @@ pub async fn validator_delegation(
 }
 
 pub async fn validator_list(api: &Api) -> Result<ValidatorListResponse> {
-    let validator_list_url = api.platform.rpc.join("validator_list").unwrap();
+    let mut conn = api.storage.lock().await.acquire().await?;
 
+    let validator_list_url = api.platform.rpc.join("validator_list").unwrap();
     debug!("validator_list_url: {}", validator_list_url);
 
     let res = api
@@ -129,10 +188,32 @@ pub async fn validator_list(api: &Api) -> Result<ValidatorListResponse> {
         .json()
         .await?;
 
+    let validator_data: ValidatorListData = serde_json::from_value(res).unwrap();
+
+    let sql = format!(
+        "SELECT address FROM block_generation WHERE height={}",
+        validator_data.cur_height.clone()
+    );
+
+    let mut wrap_validator_data = validator_data.wrap();
+
+    let rows = sqlx::query(sql.as_str()).fetch_all(&mut conn).await?;
+    let mut signers: Vec<String> = vec![];
+    for r in rows {
+        let addr: String = r.try_get("address")?;
+        signers.push(addr);
+    }
+
+    for v in &mut wrap_validator_data.validators {
+        if signers.contains(&v.addr) {
+            v.online = true
+        }
+    }
+
     Ok(ValidatorListResponse::Ok(Json(ValidatorListResult {
         code: 200,
         message: "".to_string(),
-        data: Some(res),
+        data: Some(wrap_validator_data),
     })))
 }
 
