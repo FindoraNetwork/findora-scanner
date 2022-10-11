eca479c0f6eb5b0758b7be936cb04b8ab50e8e47
dogefoodcoin@gmail.com <dogefoodcoin@gmail.com>
2022-10-11 00:57:52 -0700

    fix sql in validator_list.

--- explorer/src/service/validator.rs
+++ explorer/src/service/validator.rs
@@ -192,7 +192,7 @@ pub async fn validator_list(api: &Api) -> Result<ValidatorListResponse> {
     let validator_data: ValidatorListData = serde_json::from_value(res).unwrap();
 
     let sql = format!(
-        "SELECT address FROM block_generation WHERE height={}",
+        "SELECT address FROM block_generation WHERE height={} AND signature is not null",
         validator_data.cur_height.clone()
     );
 
